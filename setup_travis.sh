a039db922ee10502504763a8bdf29a754cda1144
Christopher Schmitt <chris.schmitt@mandiant.com>
2018-02-07 14:22:48 -0700

    Try using build flags

--- setup_travis.sh
+++ setup_travis.sh
@@ -5,7 +5,3 @@ echo $PWD
 cd /home/travis/
 git clone https://github.com/hashcat/hashcat.git && cd hashcat && git submodule update --init --recursive && git checkout v3.6.0
 make SHARED=1
-make install
-
-cp -r include/ /usr/local/include/hashcat
-cp -r deps/OpenCL-Headers/CL /usr/local/include/CL
