ec27c76feb88cf5206b693187ae6eb0ce80d45db
Christopher Schmitt <chris.schmitt@mandiant.com>
2018-02-07 14:14:02 -0700

    Move build into place

--- setup_travis.sh
+++ setup_travis.sh
@@ -3,5 +3,9 @@
 echo $PWD
 
 cd /home/travis/
-git clone https://github.com/hashcat/hashcat.git && cd hashcat && git submodule init && git submodule update && git checkout v3.6.0
+git clone https://github.com/hashcat/hashcat.git && cd hashcat && git submodule update --init --recursive && git checkout v3.6.0
 make SHARED=1
+make install
+
+cp -r include/ /usr/local/include/hashcat
+cp -r deps/OpenCL-Headers/CL /usr/local/include/CL
