de46d583d7936890f1c991f6e34cea567ca3a8f3
Christopher Schmitt <chris.schmitt@mandiant.com>
2018-02-07 14:51:57 -0700

    Use environment variables over hardcoded paths

--- setup_travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo $PWD
-
-cd /home/travis/
-git clone https://github.com/hashcat/hashcat.git && cd hashcat && git submodule update --init --recursive && git checkout v3.6.0
-make SHARED=1
