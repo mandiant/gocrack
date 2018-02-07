16afcf99817df0f60886e6bde15497ddfdb7657a
Christopher Schmitt <chris.schmitt@mandiant.com>
2018-02-07 14:10:50 -0700

    Checkout submodules

--- setup_travis.sh
+++ setup_travis.sh
@@ -3,5 +3,5 @@
 echo $PWD
 
 cd /home/travis/
-git clone https://github.com/hashcat/hashcat.git && cd hashcat && git checkout v3.6.0
+git clone https://github.com/hashcat/hashcat.git && cd hashcat && git submodule init && git submodule update && git checkout v3.6.0
 make SHARED=1
